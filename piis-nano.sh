3854b81d884ea0acb0ed5082d352e9d4b7fd131e
lryain <liuhu_0618@sina.com>
2024-11-26 15:37:49 +0800

    doly_lcd

--- piis-nano.sh
+++ piis-nano.sh
@@ -4,15 +4,12 @@ echo "Installing Python libraries..."
 apt-get install -y python3-pip python3-dev python3-pil python3-smbus libatlas-base-dev
 # WAS: pip3 install numpy pi3d==2.34 svg.path rpi-gpio adafruit-ads1x15
 apt install python3-venv
-mkdir -p /home/lryain/.env && python3 -m venv /home/lryain/.env/piis_dev
-source /home/lryain/.env/piis_dev/bin/activate
-/home/lryain/.env/piis_dev/bin/python -m pip install --upgrade pip
-pip config set global.index-url https://pypi.tuna.tsinghua.edu.cn/simple
+source /home/pi/.venv/pi3d-dev/bin/activate
+#/home/pi/.venv/pi3d-dev/bin/python -m pip install --upgrade pip
+#pip config set global.index-url https://pypi.tuna.tsinghua.edu.cn/simple
 # pi3d deps
-/home/lryain/.env/piis_dev/bin/pip install numpy pi3d svg.path rpi-gpio adafruit-blinka
-# smbus and Blinka+ADC libs are installed regardless whether ADC is
-# enabled; simplifies the Python code a little (no "uncomment this")
+/home/pi/.venv/pi3d-dev/bin/pip install numpy pi3d svg.path rpi-gpio adafruit-blinka
 
-mkdir -p /home/lryain/dev
-cd /home/lryain/dev
-git clone https://github.com/lryain/piis.git /home/lryain/dev/piis_dev
+mkdir -p /home/pi/dev
+cd /home/pi/dev
+git clone https://github.com/lryain/piis.git piis_dev
